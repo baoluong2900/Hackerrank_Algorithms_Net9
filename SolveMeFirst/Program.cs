0728d8827b7d943c0fe919f0fa543681a856fbb6
rayne <baoluong0209@gmail.com>
2025-02-23 23:52:54 +0900

    Add project files.

---
+++ SolveMeFirst/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static int solveMeFirst(int a, int b)
+    {
+        return a + b;
+    }
+
+    static void Main(string[] args)
+    {
+        int val1 = Convert.ToInt32(3);
+        int val2 = Convert.ToInt32(4);
+        int sum = solveMeFirst(val1, val2);
+        Console.WriteLine($"The sum is: {sum}");
+        Console.ReadLine();
+    }
+}
